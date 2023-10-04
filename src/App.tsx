f2d313c5bd3aef7503a16c9c1e08fe251ac382fb
Mauregina <mauraregina@gmail.com>
2023-10-04 10:16:36 -0500

    create Router file

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,16 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { BrowserRouter } from 'react-router-dom'
 
 import { ThemeProvider } from 'styled-components'
 
 import { GlobalStyle } from './styles/global'
 import { defaultTheme } from './styles/themes/default'
-import { Home } from './pages/Home'
-import { Post } from './pages/Post'
-import { DefaultLayout } from './layouts/DefaultLayout'
+import { Router } from './Router'
 
 export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
       <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<DefaultLayout />}>
-            <Route path="/" element={<Home />} />
-            <Route path="/post" element={<Post />} />
-          </Route>
-        </Routes>
+        <Router />
       </BrowserRouter>
       <GlobalStyle />
     </ThemeProvider>
